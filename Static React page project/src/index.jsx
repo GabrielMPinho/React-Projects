f4086e2f089885c2ddeb622f1ee16aac20e3829a
Gabriel Manata <gabriel.m.p0804@gmail.com>
2026-03-15 22:15:02 -0300

    feat: First react Page

--- Static React page project/src/index.jsx
+++ Static React page project/src/index.jsx
@@ -1,20 +1,11 @@
-/* 
-Dá para salvar partes do codigo (Barra de navegação, etc) em uma variiavel (<barra_nav/>) para reutilizar-la outras vezes
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-Funções devem ser em PascalCase
-*/
-import { createRoot } from "react-dom/client"
-import App from "../App"
+const root = createRoot(document.getElementById("root"));
 
-const root = createRoot(document.getElementById("root"))
-
-root.render(<App/>)
-
-
-
-
-
-
+root.render(
+    <App />
+)
 
 
 
